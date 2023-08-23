ec3907c514d559f5aaa2a99a6068d7d09df368f9
O.Berke Ozdil <ozdil@student.agh.edu.pl>
2023-08-23 23:57:09 +0300

    delete Arcade Machine program

--- Arcade Machine/number_guessing_game.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARCADE_MACHINE_NUMBER_GUESSING_GAME_H
-#define ARCADE_MACHINE_NUMBER_GUESSING_GAME_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-void numberGuessingGame();
-
-#endif //ARCADE_MACHINE_NUMBER_GUESSING_GAME_H
--- Arcade Machine/rock_paper_scissors.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARCADE_MACHINE_ROCK_PAPER_SCISSORS_H
-#define ARCADE_MACHINE_ROCK_PAPER_SCISSORS_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-void rockPaperScissors();
-
-#endif
